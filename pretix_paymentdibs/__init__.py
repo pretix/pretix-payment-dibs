3d8f4a79386bf1ed08c05e28f72d0cb6604d927a
Mikkel Ricky <rimi@aarhus.dk>
2018-03-02 14:41:08 +0100

    First commit

---
+++ pretix_paymentdibs/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_paymentdibs'
+    verbose_name = 'Credit card payment using DIBS'
+
+    class PretixPluginMeta:
+        name = ugettext_lazy('Credit card payment using DIBS')
+        author = 'Mikkel Ricky'
+        description = ugettext_lazy('Credit card payment using DIBS')
+        visible = True
+        version = '1.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_paymentdibs.PluginApp'
