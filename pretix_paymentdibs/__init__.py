5ca8d1f6359f41da1bfd0bf069ba999f96e837b8
Raphael Michel <mail@raphaelmichel.de>
2021-02-22 11:04:56 +0100

    Adjust naming and category of plugin

--- pretix_paymentdibs/__init__.py
+++ pretix_paymentdibs/__init__.py
@@ -7,10 +7,11 @@ class PluginApp(AppConfig):
     verbose_name = 'DIBS'
 
     class PretixPluginMeta:
-        name = ugettext_lazy('Card payment using DIBS')
+        name = ugettext_lazy('Nets / DIBS')
         author = 'Mikkel Ricky'
-        description = ugettext_lazy('Card payment using DIBS')
+        description = ugettext_lazy('Card payment using Scandinavic payment provider Nets / DIBS')
         visible = True
+        category = 'PAYMENT'
         version = '1.0.0'
 
     def ready(self):
