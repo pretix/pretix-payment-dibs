0ef64b22cbb0c2445cf91897eeccb789ea1e10e9
Raphael Michel <mail@raphaelmichel.de>
2022-03-22 22:32:49 +0100

    Add warning to metadata

--- pretix_paymentdibs/__init__.py
+++ pretix_paymentdibs/__init__.py
@@ -9,7 +9,7 @@ class PluginApp(AppConfig):
     class PretixPluginMeta:
         name = ugettext_lazy('Nets / DIBS')
         author = 'Mikkel Ricky'
-        description = ugettext_lazy('Card payment using Scandinavic payment provider Nets / DIBS')
+        description = ugettext_lazy('Accept card payments using Scandinavic payment provider Nets / DIBS. <span class="text-danger">Do not use this plugin for new integrations, it uses a deprecated API that will be disabled by Nets soon. Use the "nets Easy Payments" plugin instead.</span>')
         visible = True
         category = 'PAYMENT'
         version = '1.0.0'
