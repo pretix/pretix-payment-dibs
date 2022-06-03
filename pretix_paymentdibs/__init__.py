e350dc7fad260a6fc80abb7395ea2a0acf995cb6
Raphael Michel <michel@rami.io>
2022-06-03 14:02:08 +0200

    Remove deprecated imports

--- pretix_paymentdibs/__init__.py
+++ pretix_paymentdibs/__init__.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import gettext_lazy
 
 
 class PluginApp(AppConfig):
@@ -7,9 +7,9 @@ class PluginApp(AppConfig):
     verbose_name = 'DIBS'
 
     class PretixPluginMeta:
-        name = ugettext_lazy('Nets / DIBS')
+        name = gettext_lazy('Nets / DIBS')
         author = 'Mikkel Ricky'
-        description = ugettext_lazy('Accept card payments using Scandinavic payment provider Nets / DIBS. <span class="text-danger">Do not use this plugin for new integrations, it uses a deprecated API that will be disabled by Nets soon. Use the "nets Easy Payments" plugin instead.</span>')
+        description = gettext_lazy('Accept card payments using Scandinavic payment provider Nets / DIBS. <span class="text-danger">Do not use this plugin for new integrations, it uses a deprecated API that will be disabled by Nets soon. Use the "nets Easy Payments" plugin instead.</span>')
         visible = True
         category = 'PAYMENT'
         version = '1.0.0'
